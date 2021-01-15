ccb68b2a2871cc23cccf32a059d55f69e7a5a2e4
Bagula Chakravarthi <55950017+chakravarthiBagula@users.noreply.github.com>
2021-01-15 22:11:59 +0530

    Delete 16.py

--- 16.py
+++
@@ -1,8 +0,0 @@
-""" Project Euler #16: Power digit sum   """
-from math import log10, floor
-def sum_of_digits(n):
-    return floor(log10(n)) + 1
-def private_sum(n):
-    return sum_of_digits(2**n)
-for i in range(int(input())):
-    print(private_sum(int(input())))
